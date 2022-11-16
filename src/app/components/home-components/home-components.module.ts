7085ff27e1918ad69b3ccd51a5e3c3c8337fde66
Tiltu Elena <elena.tiltu04@gmail.com>
2022-11-16 22:54:01 +0200

    work on home page

--- src/app/components/home-components/home-components.module.ts
+++ src/app/components/home-components/home-components.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeroComponent } from './hero/hero.component';
-
-
+import {HeroComponent} from "./hero/hero.component";
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [
-    HeroComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeroComponent, AboutComponent],
+  imports: [CommonModule],
+  exports: [HeroComponent, AboutComponent],
 })
-export class HomeComponentsModule { }
+export class HomeComponentsModule {}
